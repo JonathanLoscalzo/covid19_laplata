b9cde403ab162043dda3c4b764fa8cbb9c238b0f
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2020-06-04 10:50:04 -0300

    add changes from 3 junio.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 python src/data/download_csv.py
 python src/data/exec_notebook.py
-jupyter nbconvert notebooks/00-viz_casos.ipynb --to html --no-input --output ../index.html
+jupyter nbconvert notebooks/00-viz_casos-output.ipynb --to html --no-input --output ../index.html
+rm notebooks/00-viz_casos-output.ipynb
--- src/data/exec_notebook.py
+++ src/data/exec_notebook.py
@@ -2,6 +2,6 @@ import papermill as pm
 
 pm.execute_notebook(
    './notebooks/00-viz_casos.ipynb',
-   './notebooks/00-viz_casos.ipynb',
+   './notebooks/00-viz_casos-output.ipynb',
    parameters=dict(datafile='./data/raw/data.csv')
 )
