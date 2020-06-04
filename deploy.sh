0516317b516240e3d16d91d754193d9873cbf50c
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2020-06-04 11:58:54 -0300

    change title with beautiful-soup

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 python3 ./deploy.py
 jupyter nbconvert ./notebooks/00-viz_casos-output.ipynb --to html --no-input --output ../index.html
 rm ./notebooks/00-viz_casos-output.ipynb
+python3 ./src/data/change_title.py
---
+++ src/data/change_title.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+filename = "./index.html"
+
+with open(filename, "r") as fp:
+    soup = BeautifulSoup(fp, "html.parser") # Or BeautifulSoup(fp, "lxml")
+    soup.title.replaceWith("COVID19 - La Plata")
+
+with open(filename, "wb") as file:
+    file.write(soup.prettify("utf-8"))
