2c0604fe11c99ce464e48931e8b4998e7e3abb79
Jonathan Loscalzo <jonathan.r.loscalzo@gmail.com>
2020-06-04 12:45:13 -0300

    fix title

--- src/data/change_title.py
+++ src/data/change_title.py
@@ -3,7 +3,7 @@ filename = "./index.html"
 
 with open(filename, "r") as fp:
     soup = BeautifulSoup(fp, "html.parser") # Or BeautifulSoup(fp, "lxml")
-    soup.title.replaceWith("COVID19 - La Plata")
+    soup.title.string.replaceWith("COVID19 - La Plata")
 
 with open(filename, "wb") as file:
     file.write(soup.prettify("utf-8"))
